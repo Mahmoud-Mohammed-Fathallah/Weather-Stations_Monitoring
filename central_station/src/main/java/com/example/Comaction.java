6ac6636a9dff35f5cddeb1e9f63ab17a36f8a634
Mahmoud-Mohammed-Fathallah <mahmodfath96@gmail.com>
2024-05-06 22:25:39 +0300

    adding read write locks and adding periodic execution for compaction

--- central_station/src/main/java/com/example/Comaction.java
+++ central_station/src/main/java/com/example/Comaction.java
@@ -11,18 +11,25 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.concurrent.locks.Lock; 
+import java.util.concurrent.locks.ReadWriteLock; 
+import java.util.concurrent.locks.ReentrantReadWriteLock; 
 
 import com.google.common.primitives.Ints;
 
 public class Comaction {
-    public static final String nameFile = "src/main/java/com/example/name.txt";
-    public static final String segmentDir = "src/main/java/com/example/bitcask/storage/";
+    public static final String nameFile = "central_station/src/main/java/com/example/name.txt";
+    public static final String segmentDir = "central_station/src/main/java/com/example/bitcask/storage/";
+    public Lock readLock;
+    public Lock writeLock;
     public int activeFile;
     public FileOutputStream outStream;
     public int byteOffset;
     public ConcurrentHashMap<Integer,Pointer> keyDir;
     public HashMap<Integer,String> mostUpdated;
-    public Comaction(ConcurrentHashMap<Integer,Pointer> map) throws IOException{
+    public Comaction(ConcurrentHashMap<Integer,Pointer> map,Lock read,Lock write) throws IOException{
+        this.readLock = read;
+        this.writeLock = write;
         // getting the name of current active file
         BufferedReader br = new BufferedReader(new FileReader(nameFile));
         this.activeFile =  Integer.parseInt(br.readLine());
@@ -33,6 +40,7 @@ public class Comaction {
         this.mostUpdated = new HashMap<>();
     }
     public void compact() throws IOException{
+        this.readLock.lock();
         System.out.println("compaction process starting...");
         for(Entry<Integer,Pointer>  e : this.keyDir.entrySet()){
             if(e.getValue().ID >= this.activeFile){
@@ -41,18 +49,21 @@ public class Comaction {
             String mostUpdatedValue = FileAccess.readRecord(e.getValue());
             this.mostUpdated.put(e.getKey(), mostUpdatedValue);
         }
+        this.readLock.unlock();
         this.writeMostUpdated();
         this.outStream.close();
         this.updateStorage();
         System.out.println("finished compaction process successfully!!!");
     }
     private void writeMostUpdated() throws IOException {
+        this.writeLock.lock();
         System.out.println("writing compacted file...");
         for(Entry<Integer,String>  e : this.mostUpdated.entrySet()){
             System.out.println("******** writing record  in file  ");
             int[] offsets = this.writeBytes(e.getKey(), e.getValue());
             this.keyDir.put(e.getKey(), new Pointer(0, offsets[0], offsets[1]-offsets[0]));
         }
+        this.writeLock.unlock();
         System.out.println("finished writing compacted file!");
     }
     public int[] writeBytes(int key, String value) throws IOException{
@@ -67,7 +78,8 @@ public class Comaction {
         offsets[1] = this.byteOffset;
         return offsets;
     }
-    public void updateStorage(){
+    public void updateStorage() throws IOException{
+        this.writeLock.lock();
         System.out.println("updating storage...");
         File dir = new File(segmentDir);
         Set<String> fileNames =  Stream.of(dir.listFiles()).filter(file -> !file.isDirectory()).map(File::getName).collect(Collectors.toSet());
@@ -79,7 +91,9 @@ public class Comaction {
         }
         // renaming the comacted file to be segment 0
         new File(segmentDir + "compacted").renameTo(new File(segmentDir+"segment-0"));
+        BitCask.writeHintFile(keyDir);
         System.out.println("storage updated successfully!!");
+        this.writeLock.unlock();
     }
 
 }
