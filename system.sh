39cd3080e93f13e9432a225011558412889396f7
abdu <abdullatifkhalid99@gmail.com>
2024-05-16 23:24:07 +0300

    update system.sh to adapt minikube and kind

--- system.sh
+++ system.sh
@@ -6,6 +6,27 @@ GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
+kind_cluster_name=$(kind get clusters)
+# Function to determine if Minikube or Kind is used
+determine_env() {
+    if minikube status >/dev/null 2>&1; then
+        echo "minikube"
+        alias kubectl="minikube kubectl --"
+    elif kind get clusters >/dev/null 2>&1; then
+        echo "kind"
+        
+    else
+        echo "none"
+    fi
+}
+
+# Set environment
+ENV=$(determine_env)
+if [ "$ENV" == "none" ]; then
+    echo -e "${RED}Neither Minikube nor Kind (with cluster name 'wsm') is running. Please start one of them and try again.${NC}"
+    exit 1
+fi
+
 # Function to build the producer jar
 build_producer_jar() {
     echo -e "${GREEN}Building the jar of the producer${NC}"
@@ -13,7 +34,7 @@ build_producer_jar() {
 }
 
 # Function to build the Streaming jar
-build_Streaming_jar() {
+build_streaming_jar() {
     echo -e "${GREEN}Building the jar of the streaming${NC}"
     mvn clean install package
 }
@@ -27,35 +48,49 @@ build_central_station_jar() {
 # Function to build the producer image
 build_producer_image() {
     echo -e "${GREEN}Building the docker image of the producer${NC}"
-    sudo docker build -t weather-station -f DockerfileProducer .
+     docker build -t weather-station -f DockerfileProducer .
     echo -e "${GREEN}Loading the docker image of the producer${NC}"
-    minikube image load weather-station:latest
+    if [ "$ENV" == "minikube" ]; then
+        minikube image load weather-station:latest
+    elif [ "$ENV" == "kind" ]; then
+        kind load docker-image weather-station:latest --name $kind_cluster_name
+    fi
 }
 
 # Function to build the streaming image
 build_streaming_image() {
     echo -e "${GREEN}Building the docker image of the streaming${NC}"
-    sudo docker build -t kafkastreaming -f DockerfileStreaming .
+     docker build -t kafkastreaming -f DockerfileStreaming .
     echo -e "${GREEN}Loading the docker image of the streaming${NC}"
-    minikube image load kafkastreaming:latest
+    if [ "$ENV" == "minikube" ]; then
+        minikube image load kafkastreaming:latest
+    elif [ "$ENV" == "kind" ]; then
+        kind load docker-image kafkastreaming:latest --name $kind_cluster_name
+    fi
 }
-
-
 
 # Function to build the central station image
 build_central_station_image() {
     echo -e "${GREEN}Building the docker image of the central station${NC}"
-    sudo docker build -t central-station -f DockerfileConsumer .
+     docker build -t central-station -f DockerfileConsumer .
     echo -e "${GREEN}Loading the docker image of the central station${NC}"
-    minikube image load central-station:latest
+    if [ "$ENV" == "minikube" ]; then
+        minikube image load central-station:latest
+    elif [ "$ENV" == "kind" ]; then
+        kind load docker-image central-station:latest --name $kind_cluster_name
+    fi
 }
 
 # Function to build the uploader image
 build_uploader_image() {
     echo -e "${GREEN}Building the docker image of the uploader${NC}"
-    sudo docker build -t upload-app .
+     docker build -t upload-app .
     echo -e "${GREEN}Loading the docker image of the uploader${NC}"
-    minikube image load upload-app:latest
+    if [ "$ENV" == "minikube" ]; then
+        minikube image load upload-app:latest
+    elif [ "$ENV" == "kind" ]; then
+        kind load docker-image upload-app:latest --name $kind_cluster_name
+    fi
 }
 
 # Function to bring up the system
@@ -71,7 +106,7 @@ up() {
 
         # Build the Streaming jar
         cd ../Streaming  
-        build_Streaming_jar
+        build_streaming_jar
 
         cd ..
     fi
@@ -99,25 +134,25 @@ up() {
     cd K8S
     # Deploy Kafka
     echo -e "${GREEN}Deploying Kafka${NC}"
-    minikube kubectl -- apply -f kafka.yml
+    kubectl apply -f kafka.yml
     # Deploy Streaming
     echo -e "${GREEN}Deploying Streaming${NC}"
-    minikube kubectl -- apply -f KafkaProcessor.yml
+    kubectl apply -f KafkaProcessor.yml
     # Deploy the storage
     echo -e "${GREEN}Deploying the storage${NC}"
-    minikube kubectl -- apply -f storage.yml
+    kubectl apply -f storage.yml
     # Deploy ten producers
     echo -e "${GREEN}Deploying the producers${NC}"
     ./run10.sh
     # Deploy the central station
     echo -e "${GREEN}Deploying the central station${NC}"
-    minikube kubectl -- apply -f central_station.yml
+    kubectl apply -f central_station.yml
     # Deploy the uploader
     echo -e "${GREEN}Deploying the uploader${NC}"
-    minikube kubectl -- apply -f upload_parquets.yml
+    kubectl apply -f upload_parquets.yml
     # Deploy the ELK stack
     echo -e "${GREEN}Deploying the ELK stack${NC}"
-    minikube kubectl -- apply -f Elk.yml
+    kubectl apply -f Elk.yml
 }
 
 # Function to bring down the system
@@ -126,19 +161,19 @@ down() {
     cd K8S
     # Delete the ELK stack
     echo -e "${RED}Deleting the ELK stack${NC}"
-    minikube kubectl -- delete -f Elk.yml
+    kubectl delete -f Elk.yml
     # Delete the uploader
     echo -e "${RED}Deleting the uploader${NC}"
-    minikube kubectl -- delete -f upload_parquets.yml
+    kubectl delete -f upload_parquets.yml
     # Delete the central station
     echo -e "${RED}Deleting the central station${NC}"
-    minikube kubectl -- delete -f central_station.yml
+    kubectl delete -f central_station.yml
     # Delete Kafka
     echo -e "${RED}Deleting Kafka${NC}"
-    minikube kubectl -- delete -f kafka.yml
+    kubectl delete -f kafka.yml
     # Delete Streaming
     echo -e "${RED}Deleting Streaming${NC}"
-    minikube kubectl -- delete -f KafkaProcessor.yml
+    kubectl delete -f KafkaProcessor.yml
     # Delete the producers
     echo -e "${RED}Deleting the producers${NC}"
     ./stop10.sh
